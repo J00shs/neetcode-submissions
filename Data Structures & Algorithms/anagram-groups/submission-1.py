06884ed875f869a419cb4491b414bfa4d9d94e59
J00shs <75855582+J00shs@users.noreply.github.com>
2026-09-01 13:26:22 -0400

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,30 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # Create the hashmap
+        # Mapping charCount to the list of anagrams
+        res = defaultdict(list)
+
+        for s in strs:
+            # Create an array with 26 characters (a-z)
+            count = [0] * 26
+
+            # For each letter in a string
+            for c in s:
+                # How can we map a to index 0 and z to index 25?
+                # Take the ASCII value of each character - ASCII value of a
+
+                # Example:
+                # a = 97
+                # b = 98
+                # 98 - 97 = 1
+                # Therefore, b is stored at index 1
+                count[ord(c) - ord("a")] += 1
+
+            # Use the completed character count as the key
+            res[tuple(count)].append(s)
+
+        return list(res.values())
+
+        # O(m * n) solution
+        # m = number of strings given
+        # n = average number of characters in each string
